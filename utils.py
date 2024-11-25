27750605736db46b7b58aa1cc5fd8c6e95be7ff7
Julian Jungnitz <julian@MacBook-Pro.local>
2024-11-25 11:15:55 +0100

    changed config for hospital run

--- utils.py
+++ utils.py
@@ -1,21 +1,22 @@
 import os
 
-NEO4J_URL = "bolt://localhost:7687"
-NEO4J_LOGIN = "neo4j"
-NEO4J_PASSWORD = "password"
-NEO4J_DATABASE = "neo4j"
+
+NEO4J_URI = "bolt://172.19.0.3:7687"
+NEO4J_USERNAME = "neo4j"
+NEO4J_PASSWORD = "Neo4JJJJJ"
+NEO4J_DB = "graphdb2"
 
 LOG_FILE = "./log.txt"
 
 FEATURES_FOLDER = "./patient_features"
-percentage_sections = [-1,-0.75, -0.5, -0.25, -0.1, 0, 0.1, 0.25, 0.5, 0.75, 1]
-
+percentage_sections = [-1, -0.75, -0.5, -0.25, -0.1, 0, 0.1, 0.25, 0.5, 0.75, 1]
 
 
 def log_to_file(message):
     print(message)
     with open(LOG_FILE, "a") as f:
         f.write(message)
+
 
 def clear_log_file():
     with open(LOG_FILE, "w") as f:
@@ -26,9 +27,11 @@ def request(driver, query):
     with driver.session(database=NEO4J_DATABASE) as session:
         result = session.run(query).data()
         return result
-    
+
+
 def get_disease_folder_name(disease_name):
     return f"{FEATURES_FOLDER}/{disease_name}"
+
 
 def save_feature_analysis_to_file(
     disease_name,
@@ -37,7 +40,7 @@ def save_feature_analysis_to_file(
     disease_count,
     node_type,
     relationship_type,
-    ):
+):
     folder_name = get_disease_folder_name(disease_name)
     features_file_name = f"{folder_name}/{relationship_type}_{node_type}.csv"
     general_file_name = f"{folder_name}/general.csv"
